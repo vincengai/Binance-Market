7722144138626671d098c25e5e93b98b28f3c3c9
Vince <53957319+vincengai@users.noreply.github.com>
2020-01-07 17:11:35 -0800

    Fix route for static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     resources :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
   end
-  root "static_pages#root"
+  root to: "static_pages#root"
 end
