8d5071a9fd34d71d9dbda1acce6a59878f9be078
Vince <53957319+vincengai@users.noreply.github.com>
2020-01-10 15:00:39 -0800

    Fixed WalleController

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,12 @@
 class Wallet < ApplicationRecord
-    validates :name, :wallet_address, presence: true 
+    validates :user_id, :coin_id, :symbol, :name, presence: true 
+
+    belongs_to :user, 
+    foreign_key: :user_id,
+    class_name: :User
+
+    has_many :coins, 
+    foreign_key: :coin_id,
+    class_name: :Coin
+
 end
