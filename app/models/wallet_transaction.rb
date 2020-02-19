7f437da59558ad3f16c5ee6af2998d9c04696457
Vince <53957319+vincengai@users.noreply.github.com>
2020-02-19 15:23:46 -0800

    typo in trans model

--- app/models/wallet_transaction.rb
+++ app/models/wallet_transaction.rb
@@ -1,6 +1,6 @@
 class WalletTransaction < ApplicationRecord
     validates :wallet_id, :user_id, presence: true
-    validates :transaction_type, includsion: {in: ['buy', 'sell']}
+    validates :transaction_type, inclusion: {in: ['buy', 'sell']}
 
     belongs_to :coin
 
