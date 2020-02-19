357cf0442c1a69b4bd078e6b30fe3e02731b358b
Vince <53957319+vincengai@users.noreply.github.com>
2020-02-19 15:19:46 -0800

    fixed syntax in walletTrans model

--- app/models/wallet_transaction.rb
+++ app/models/wallet_transaction.rb
@@ -2,9 +2,9 @@ class WalletTransaction < ApplicationRecord
     validates :wallet_id, :user_id, presence: true
     validates :transaction_type, includsion: {in: ['buy', 'sell']}
 
-    belongs_to :coin,
+    belongs_to :coin
 
-    belongs_to :user,
+    belongs_to :user
 
     
 end
