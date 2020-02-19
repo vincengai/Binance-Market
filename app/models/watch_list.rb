1b6562ccaca4b411a236719dc19c043fee56009a
Vince <53957319+vincengai@users.noreply.github.com>
2020-02-19 15:38:30 -0800

    debugging watchlist

--- app/models/watch_list.rb
+++ app/models/watch_list.rb
@@ -1,6 +1,6 @@
 class WatchList < ApplicationRecord
     validates :user_id, :coin_id, presence: true 
-    validates :coin_id, uniqueness {scope: :user_id, message: "User already has this coin in their watchlist"}
+    validates :coin_id, uniqueness: {scope: :user_id, message: "User already has this coin in their watchlist"}
 
     belongs_to :user,
     foreign_key: :user_id, 
