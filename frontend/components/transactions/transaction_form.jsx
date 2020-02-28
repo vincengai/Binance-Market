cb58677d3af26f90dddc2415aff30daeb44e1f21
Vince <53957319+vincengai@users.noreply.github.com>
2020-02-27 16:13:14 -0800

    updated modal styling

--- frontend/components/transactions/transaction_form.jsx
+++ frontend/components/transactions/transaction_form.jsx
@@ -18,19 +18,55 @@ class TransactionForm extends React.Component {
         });
     }
 
+
+
     render() {
         return (
-            <div className="trade-widget">
-                <div className="trade-top"> 
-                    <div> Sell </div>
-                    <div> Buy </div>
+            // <div className="trade-widget">
+            //     <div className="trade-top"> 
+            //         <div> Sell </div>
+            //         <div> Buy </div>
+            //     </div>
+
+            //     <div className="trade-middle">
+            //         <input type="number" placeholder='0 units' minLength='1' onChange={this.update('units')} />
+            //     </div> 
+
+            // </div>        
+
+            <div className='widget-trade'>
+                <div className='trade-top'>
+                    {/* <div className='active' onClick={this.handleOrderClick("buy")}>Buy</div>
+                    <div onClick={this.handleOrderClick("sell")}>Sell</div>
+                    <div onClick={this.handleOrderClick("convert")}>Convert</div>  */}
+
+                     <div>Sell</div>
+                     <div> Buy </div>
                 </div>
-
-                <div className="trade-middle">
+                <div className='trade-middle'>
                     <input type="number" placeholder='0 units' minLength='1' onChange={this.update('units')} />
-                </div> 
+                    <div>
+                        {/* {errors go here} */}
+                    </div>
 
-            </div>        
+
+                    <div className='trade-crypto-selector'>
+                        <p>
+                            Buy
+                        </p>
+                        <div>
+                            <div>
+                                {/* <img src={imageSource} /> */}
+                            </div>
+                        </div>
+                    </div>
+                    <button type='button'>
+                        hello{/* {transaction_type.slice(0, 1).toUpperCase().concat(transaction_type.slice(1))} {this.state.name} */}
+                    </button>
+                </div>
+                <div className='trade-bottom'>
+                </div>
+            </div>
     )};
 }
 
