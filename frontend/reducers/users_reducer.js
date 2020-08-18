2b139039d32216e0e2f2877509f67e9602b3305e
Vince <53957319+vincengai@users.noreply.github.com>
2020-08-18 14:56:38 -0700

    updated login feature

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -8,7 +8,7 @@ const usersReducer = (oldState = {}, action) => {
 
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return Object.assign({}, oldState, { [action.data.currentUser_id]: action.currentUser });
+            return Object.assign({}, oldState, { [action.currentUser.id]: action.currentUser });
         
         case RECEIVE_BUY_INFO:
 			// action.userData ==  { id: 17, email: 'demo@gmail.com', cash_balance: 3000, portfolio: {'BTC': 1} }
