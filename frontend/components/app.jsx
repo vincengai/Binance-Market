5298953f6a94abfe60c8d6e92159ce802ad1fdb1
Vince <53957319+vincengai@users.noreply.github.com>
2020-09-28 17:05:14 -0700

    deleting dashboard

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import SignupFormContainer from './session_forms/signup_form_container';
 import LoginFormContainer from './session_forms/login_form_container';
 import CryptoIndexContainer from './crypto/crypto_index_container';
+import CryptoDashboardContainer from './dashboard/dashboard_constainer'
 import CryptoShowContainer from './crypto/crypto_show_container';
 import Splash from './splash';
 import Foot from './footer';
@@ -21,6 +22,8 @@ const App = () => (
             <Route exact path="/signup" component={SignupFormContainer} />
             <Route exact path="/coins" component={CryptoIndexContainer} />
             <Route exact path="/coins/:symbol" component={CryptoShowContainer} />
+            {/* <Route exact path="/wallet" component={CryptoDashboardContainer} /> */}
+            
             <Route exact path="/" component={Splash} />
         </Switch>
 
--- frontend/components/dashboard/dashboard_constainer.jsx
+++ frontend/components/dashboard/dashboard_constainer.jsx
@@ -1,4 +1,4 @@
-import { connect } from 'react-redux';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import CryptoIndex from './crypto_index';
+// import { connect } from 'react-redux';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import CryptoIndex from './crypto_index';
