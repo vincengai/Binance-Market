7edf9b1cbe4fcb935c4da39abe040beaacf62597
Vince <53957319+vincengai@users.noreply.github.com>
2020-10-26 15:16:36 -0700

    adding google analytics

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,14 +4,22 @@ import { Route, Switch } from 'react-router-dom';
 import SignupFormContainer from './session_forms/signup_form_container';
 import LoginFormContainer from './session_forms/login_form_container';
 import CryptoIndexContainer from './crypto/crypto_index_container';
-import CryptoDashboardContainer from './dashboard/dashboard_constainer'
+import CryptoDashboardContainer from './dashboard/dashboard_container'
 import CryptoShowContainer from './crypto/crypto_show_container';
 import Splash from './splash';
 import Foot from './footer';
 import Modal from '../components/crypto/modal';
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+  ReactGA.initialize("G-RF73WL6GWF");
+  ReactGA.pageview("/homepage");
+}
+
 
 const App = () => (
     <div>
+    {initializeReactGA()}
         <Modal />
         <header>
             <NavBarContainer />
