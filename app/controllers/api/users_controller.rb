b06ae2663bd7653b7d59f69ad0a64bbf2ecdb606
Vince <53957319+vincengai@users.noreply.github.com>
2020-11-05 13:03:52 -0800

    updating css stylesheed for dashboard

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-    @user.cash_balance = 100000
+    @user.cash_balance = 1000000
 
     if @user.save
       login(@user)
