42f30315ba3944e3ad95a1739707487dc3147a38
Vince <53957319+vincengai@users.noreply.github.com>
2021-03-02 17:38:06 -0800

    precompiling assets pt2

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,7 +3,7 @@ import NavBarContainer from './navbars/navbar_container';
 import { Route, Switch } from 'react-router-dom';
 import SignupFormContainer from './session_forms/signup_form_container';
 import LoginFormContainer from './session_forms/login_form_container';
-import CryptoIndexContainer from './crypto/crypto_index_container';
+// import CryptoIndexContainer from './crypto/crypto_index_container';
 import DashboardContainer from './dashboard/dashboard_container';
 import CryptoShowContainer from './crypto/crypto_show_container';
 import Splash from './splash';
@@ -21,7 +21,7 @@ const App = () => (
         <Switch>
             <Route exact path="/login" component={LoginFormContainer} />
             <Route exact path="/signup" component={SignupFormContainer} />
-            <Route exact path="/coins" component={CryptoIndexContainer} />
+            {/* <Route exact path="/coins" component={CryptoIndexContainer} /> */}
             <Route exact path="/coins/:symbol" component={CryptoShowContainer} />
             <Route exact path='/dashboard' component={DashboardContainer} />
             {/* <Route exact path="/wallet" component={CryptoDashboardContainer} /> */}
--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,105 +1,35 @@
 import React from 'react';
 import { Link, NavLink, Route } from 'react-router-dom';
-// import CryptoIndexContainer from './crypto/crypto_index_container';
-import CryptoSplashIndexContainer from './crypto/crypto_splash_index_constainer';
-// import PaperTradeAdvert from './advert/papertrading_advert';
+import CryptoSplashIndex from './crypto/crypto_splash_index';
 /*eslint-disable*/
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
+
 // core components
 
 import GridContainer from "./advert/components/Grid/GridContainer";
 import GridItem from "./advert/components/Grid/GridItem.js";
-import Button from "./button/Button.js";
-import Header from "./Header/Header.js";
-import HeaderLinks from "./Header/HeaderLinks";
-import Footer from "../components/Footer/Footer";
+
 import Parallax from "./Parallax/Parallax";
 
 
 import SectionDescription from "./views/SectionDescription.js";
-// Everythign below has to be made
-// sections for this page
-import SectionComponents from "./views/SectionComponents.js";
-// import SectionCards from "./views/SectionCards.js";
-// import SectionContent from "./views/SectionContent.js";
-// import SectionSections from "./views/SectionSections.js";
-// import SectionExamples from "./views/SectionExamples.js";
 
-// Still have yet to add in the bottom files
+
+import SectionComponents from "./views/SectionComponents.js";
+
+
 import SectionOverview from "./views/SectionOverview.js";
-// import SectionPricing from "./views/SectionPricing.js";
+
 
 import presentationStyle from "../../app/assets/jss/material-kit-pro-react/views/presentationStyle.js";
-import steller from '../../app/assets/images/steller.gif'
 const useStyles = makeStyles(presentationStyle);
 
 const COINS = [
         { name: "Bitcoin", symbol: "BTC"}
 ]
-
-// class Splash extends React.Component {
-
-
-//     render() {
-//         return (
-//           <div className="splash-page">
-//             <div className="splash-container">
-//               <div className="splash-sent">
-//                 The World's Most Trustworthy
-//                 <br />
-//                 Cryptocurrency Exchange
-//               </div>
-//               <div className="splash-subsent">
-//                 Trade Bitcoin, BNB, and hundreds of other cryptocurrencies in
-//                 minutes.{" "}
-//               </div>
-
-//               <div className="splash-subsent2">I want to spend</div>
-//               <div className="splash-subsent3">I want to buy</div>
-
-//               <img src={window.imageUrl.advertise} className="ad-logo" />
-//               <NavLink to="/">
-//                 <div className="splash-logo"></div>
-//               </NavLink>
-
-//               <input
-//                 type="number"
-//                 placeholder="Enter amount"
-//                 className="splash-amount"
-//                 minLength='1' maxLength='6'
-//               />
-//               <input
-//                 type="number"
-//                 placeholder="Enter amount"
-//                 className="splash-buy"
-//                 minLength='1' maxLength='6'
-//               />
-//               <NavLink className="buy-button" to="/coins/BTC">
-//                 Buy BTC
-//               </NavLink>
-//             </div>
-
-//             <a href="https://vincengai.github.io/Currency-Compare/" className="currency-compare">
-//               <div className="cur-comp-link">
-//                 <img src={window.imageUrl.megaphone} className='megaphone'/>
-//                 Need advice investing? Come checkout my project Currency Compare
-//               </div>
-//             </a>
-
-//             {/* <PaperTradeAdvert /> */}
-//             <CryptoSplashIndexContainer />
-//           </div>
-    
-//         );
-//     }
-// };
-
-// export default Splash; 
 
 
 export default function Splash() {
@@ -128,7 +58,7 @@ export default function Splash() {
           </div>
         </Parallax>
         <div className={classNames(classes.main, classes.mainRaised)}>
-          <CryptoSplashIndexContainer />
+          <CryptoSplashIndex />
           <SectionOverview />
           <SectionDescription />
         </div>
